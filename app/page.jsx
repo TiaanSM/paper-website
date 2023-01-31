4a52aad596037583fd7010b4479cc60207ea8572
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2023-01-31 13:46:49 +0200

    Completed basic open menu layout.
    Completed the basic layout for when the hamburger is pressed, added link hover animations, open and close fade animation and open and close functionality.

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <main>
       <h1>Hello Next</h1>
+      <div className={styles.dummy}></div>
+      <div className={styles.dummy}></div>
+      <div className={styles.dummy}></div>
     </main>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,16 +7,21 @@ import { useState, useEffect } from 'react'
 const Navbar = () => {
 
     const [isActive, setIsActive] = useState(false);
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsActive(!isActive);
+        setOpenMenu(!openMenu);
+    }
     
   return (
     <div className={styles.nav}>
-        <div>
-            <span>Paper.</span>
-        </div>
 
-        <span>
-            <p>DIGITAL SOUL &copy; 2022</p>
-        </span>
+      <div className={styles.contentContainer}>
+        
+        <span className={styles.logo}>Paper<span className={styles.reg}>&reg;</span></span>
+        
+        <span className={styles.centerText}>DIGITAL SOUL &copy; 2022</span>
 
         <div className={styles.rightContainer}>
 
@@ -24,20 +29,60 @@ const Navbar = () => {
                 <span>Let's talk</span>
             </div>
 
-            <div onClick={() => setIsActive(!isActive)} 
+            <div onClick={handleMenuClick} 
                 className={isActive ? styles.activeMenu : styles.menu}>
                 <span className={isActive ? styles.activeBar1 : styles.bar1}></span>
                 <span className={isActive ? styles.activeBar2 : styles.bar2}></span>
             </div>
+       
+        </div>
+
+      </div>
+   
+      <div className={openMenu ? styles.openMenu : styles.closedMenu}>
+
+        <div className={styles.mainMenuContainer}> 
+
+            <div className={styles.leftMenuContainer}>
+                <div className={styles.bigLogoContainer}>
+                    <span className={styles.bigLogo}>P\</span>
+                </div>
+                <ul className={styles.socialsContainer}>
+                    <li className={styles.socialLink}>Behance</li>
+                    <li className={styles.socialLink}>Twitter</li>
+                    <li className={styles.socialLink}>Instagram</li>
+                    <li className={styles.socialLink}>Linkdin</li>
+                    <li className={styles.socialLink}>Medium</li>
+                </ul>
+            </div>
+ 
+            <div className={styles.rightMenuContainer}>
+                <ul className={styles.linkContainer}>
+                    <li className={styles.navLink}>Work</li>
+                    <li className={styles.navLink}>Services</li>
+                    <li className={styles.navLink}>About</li>
+                    <li className={styles.navLink}>Manifesto</li>
+                    <li className={styles.navLink}>Contact</li>
+                </ul>
+            </div>
 
         </div>
+
+        <div className={styles.footerMenuContainer}>
+
+                <span className={styles.footerLinkContainer}>
+                    <p className={styles.footerLink}>Privacy policy</p>
+                    <p className={styles.footerLink}>Cookies policy</p>
+                    <p className={styles.footerLink}>Terms & Conditions</p>
+                </span>
+
+                <span className={styles.emailLink}>hello@paper.fakemail</span>
+        </div>
+
+      </div>
+
     </div>
   )
 }
 
 export default Navbar
-
-/*
-
- 
-*/
